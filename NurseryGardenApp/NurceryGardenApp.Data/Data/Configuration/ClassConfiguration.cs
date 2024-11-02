08fa72e358674e44501ef50499a6599f8240d084
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-11-02 17:07:34 +0200

    Added SeedClasses method and implement Configure method

--- NurseryGardenApp/NurceryGardenApp.Data/Data/Configuration/ClassConfiguration.cs
+++ NurseryGardenApp/NurceryGardenApp.Data/Data/Configuration/ClassConfiguration.cs
@@ -13,7 +13,54 @@ namespace NurseryGardenApp.Data.Data.Configuration
 	{
 		public void Configure(EntityTypeBuilder<Class> builder)
 		{
-			throw new NotImplementedException();
+			builder.HasData(this.SeedClasses());
+		}
+
+		private IEnumerable<Class> SeedClasses()
+		{	
+			IEnumerable<Class> classes = new List<Class>()
+			{
+				new Class()
+			{
+				//Широколистни
+				Id = 1,
+				Name = "Deciduous"
+			},
+
+			new Class()
+			{	
+				//Иглолистни
+				Id = 2,
+				Name = "Coniferous"
+			},
+
+			new Class()
+			{   
+				Id = 3,
+				Name = "Fruity"
+			},
+
+			new Class()
+			{	
+				Id = 4,
+				Name = "Garden Flowers"
+			},
+
+			new Class()
+			{
+				Id = 5,
+				Name = "Indoor Flowers"
+			},
+
+			new Class()
+			{
+				Id = 6,
+				Name = "Seeds"
+			}
+
+			};
+
+			return classes;
 		}
 	}
 }
