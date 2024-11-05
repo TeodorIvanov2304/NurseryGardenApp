38b6fe1fb3b477601b57d7d03d5b8db6ae1283a3
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-11-05 12:44:23 +0200

    Added CategoryConfiguration and seed categories

---
+++ NurseryGardenApp/NurceryGardenApp.Data/Data/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NurseryGardenApp.Data.Models;
+
+namespace NurseryGardenApp.Data.Data.Configuration
+{
+	public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+	{
+		public void Configure(EntityTypeBuilder<Category> builder)
+		{
+			builder.HasData(this.SeedCategories());
+		}
+
+		private  IEnumerable<Category> SeedCategories()
+		{
+			IEnumerable<Category> categories = new List<Category>
+			{
+				new Category
+				{
+					Id = 1,
+					Name = "Trees"
+				},
+				new Category
+				{
+					Id = 2,
+					Name = "Bushes"
+				},
+				new Category
+				{
+					Id = 3,
+					Name = "Flowers"
+				},
+				new Category
+				{
+					Id = 4,
+					Name = "Seedlings"
+				},
+				new Category
+				{
+					Id = 5,
+					Name = "Garden"
+				}
+				,
+				new Category
+				{
+					Id = 6,
+					Name = "Evergreens"
+				}
+			};
+
+			return categories;
+		}
+			
+			
+	}
+}
---
+++ NurseryGardenApp/NurceryGardenApp.Data/Data/Migrations/20241105103027_SeedCategoryData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NurseryGardenApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "ClassId", "Name" },
+                values: new object[,]
+                {
+                    { 1, null, "Trees" },
+                    { 2, null, "Bushes" },
+                    { 3, null, "Flowers" },
+                    { 4, null, "Seedlings" },
+                    { 5, null, "Garden" },
+                    { 6, null, "Evergreens" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
