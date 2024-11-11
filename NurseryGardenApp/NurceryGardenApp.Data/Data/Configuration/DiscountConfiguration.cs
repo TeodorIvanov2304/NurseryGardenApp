33159cc2f4b2a6a2bb152d4f2235528f3105ca32
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-11-11 23:39:16 +0200

    Update DiscountConfiguration
    Add SeedDiscounts method for seeding discounts in the DB

--- NurseryGardenApp/NurceryGardenApp.Data/Data/Configuration/DiscountConfiguration.cs
+++ NurseryGardenApp/NurceryGardenApp.Data/Data/Configuration/DiscountConfiguration.cs
@@ -11,6 +11,33 @@ namespace NurseryGardenApp.Data.Data.Configuration
 			builder.Property(p => p.DiscountValue)
 				   .HasColumnType("decimal")
 				   .HasPrecision(18, 2);
+
+			builder.HasData(this.SeedDiscounts());
+		}
+
+		private IEnumerable<Discount> SeedDiscounts()
+		{
+			IEnumerable<Discount> discounts = new List<Discount>()
+			{
+				new Discount()
+				{
+					Id = 1,
+					Name = "25 percent discount",
+					DiscountValue = 25.00m,
+					StartDate = DateTime.Today,
+					EndDate = DateTime.Today.AddYears(1)
+				},
+				new Discount()
+				{
+					Id = 2,
+					Name = "10 percent discount",
+					DiscountValue = 10.00m,
+					StartDate = DateTime.Today,
+					EndDate = DateTime.Today.AddYears(1)
+				}
+			};
+
+			return discounts;
 		}
 	}
 }
