598fe4384eed7cfa17f0320811628048bb4a5d79
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-11-12 17:24:09 +0200

    Created Repository folder
    Created Repository folder and folder Interfaces, added IRepository interface

---
+++ NurseryGardenApp/NurceryGardenApp.Data/Data/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace NurseryGardenApp.Data.Data.Repositories.Interfaces
+{
+	public interface IRepository<TType,TId> where TType : class where TId : struct
+	{
+		TType GetById(TId id);
+		Task<TType> GetByIdAsync(TId id);
+		IEnumerable<TType> GetAll();
+		Task<IEnumerable<TType>> GetAllAsync();
+
+
+		void Add(TType item);
+		Task AddAsync(TType item);
+
+
+		//If we want soft delete, we have to pass (id,true)
+		bool Delete(TId id, bool softDelete = false);
+		Task<bool> DeleteAsync(TId id, bool softDelete = false);
+
+
+		bool Update(TType item);
+		Task<bool> UpdateAsync(TType item);
+
+
+		int SaveChanges();
+		Task<int> SaveChangesAsync();
+	}
+}
