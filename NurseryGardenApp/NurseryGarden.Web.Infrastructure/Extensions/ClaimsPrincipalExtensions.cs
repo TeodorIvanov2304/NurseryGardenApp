e3dd7ba151aa4ecb7bb6c79404ebfefc876cc489
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-12-12 15:30:37 +0200

    Fixed major error, where non-manager users sees Delete and Edit buttons

--- NurseryGardenApp/NurseryGarden.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
+++ NurseryGardenApp/NurseryGarden.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -2,8 +2,8 @@
 
 namespace NurseryGarden.Web.Infrastructure.Extensions
 {
-    public static class ClaimsPrincipalExtensions
-    {
+	public static class ClaimsPrincipalExtensions
+	{
 		public static string? GetUserId(this ClaimsPrincipal? userClaimsPrincipal)
 		{
 			return userClaimsPrincipal?.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? null;
